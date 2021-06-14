cb7c80f14fb2184a9a3878fd02ae08c47023dced
David Gwartney <david.gwartney@kore.com>
2021-06-14 11:06:49 -0700

    checkpoint

--- app.js
+++ app.js
@@ -8,12 +8,4 @@ var server = new Server(config, app);
 
 server.start();
 
-//sdk.registerBot(require('./FindAFlight.js'));
-//sdk.registerBot(require('./SimpleConversationalBot.js'));
-//sdk.registerBot(require('./SimpleConversationalBotWithMultipleBotId.js'));
-//sdk.registerBot(require('./GuessTheNumber.js'));
-//sdk.registerBot(require('./BookACab.js'));
-//sdk.registerBot(require('./OrderAPizza.js'));
-//sdk.registerBot(require('./BotVariables.js'));
-//sdk.registerBot(require('./LiveChat.js'));
 sdk.registerBot(require('./Hook.js'));
--- echo.js
+++ echo.js
@@ -1,57 +1,26 @@
-let sdk = require("./lib/sdk");
-let Promise = sdk.Promise;
-let request = require("request");
-let config = require("./config");
+const sdk = require("./lib/sdk");
+const Promise = sdk.Promise;
+const config = require("./config");
+const request = require("request");
 const log4js = require("log4js");
 const logger = log4js.getLogger();
 logger.level = "DEBUG";
-
-
-let amdApiKey = "<%amdApiKey%>";
-
-//Make request to service app
-function getNutritionInformation() {
-    return new Promise(function (resolve, reject) {
-        request({
-            url: "https://www.tapintoyourbeer.com/api/v1/beers",
-            method: 'get'
-        }, function (err, res) {
-            if (err) {
-                return reject(err);
-            }
-            resolve(JSON.parse(res.body));
-        });
-    });
-}
-
-
-//Make request to service app
-function findAirports(searchTerm) {
-    var airportsUrl = "http://api.sandbox.amadeus.com/v1.2/airports/autocomplete?apikey=" + amdApiKey;
-    return new Promise(function (resolve, reject) {
-        request({
-            url: airportsUrl + "&term=" + searchTerm,
-            method: 'get',
-        }, function (err, res) {
-            if (err) {
-                return reject(err);
-            }
-            resolve(JSON.parse(res.body));
-        });
-    });
-}
-
 
 module.exports = {
     botId: config.bot.id,
     botName: config.bot.name,
     on_user_message: function (requestId, data, callback) {
         logger.debug(`on_user_message()=> requestId:${requestId}, data.message: ${data.message}`);
-        sdk.sendBotMessage(data, callback);
+        if (data.message === 'Hello') {
+            data.message = "Hi, How are you?";
+            return sdk.sendUserMessage(data, callback);
+        } else {
+            return sdk.sendBotMessage(data, callback);
+        }
     },
     on_bot_message: function (requestId, data, callback) {
         logger.debug(`on_bot_message()=> requestId:${requestId}, data.message: ${data.message}`);
-        sdk.sendUserMessage(data, callback);
+        return sdk.sendUserMessage(data, callback);
     },
     on_webhook: function (requestId, data, componentName, callback) {
         logger.debug(`on_bot_message()=> requestId:${requestId}, componentName: ${componentName}, data.message: ${data.message}`);
